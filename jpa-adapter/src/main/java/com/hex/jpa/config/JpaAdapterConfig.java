15da10451e503081b97853413a2a89801f9bb106
Deb <debdutta94ofc@gmail.com>
2020-12-17 00:15:22 +0530

    refactor: fixed issue with build

--- jpa-adapter/src/main/java/com/hex/jpa/config/JpaAdapterConfig.java
+++ jpa-adapter/src/main/java/com/hex/jpa/config/JpaAdapterConfig.java
@@ -1,11 +1,20 @@
 package com.hex.jpa.config;
 
+import com.hex.jpa.StudentRepository;
+import com.hex.jpa.dao.StudentDao;
+import com.hexdomain.api.port.ObtainStudent;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @Configuration
-@EntityScan("com.hex.jpa.entity")
-@EnableJpaRepositories("com.hex.jpa.dao")
+@EntityScan(basePackages = {"com.hex.jpa.entity"})
+@EnableJpaRepositories(basePackages = {"com.hex.jpa.dao"})
 public class JpaAdapterConfig {
+
+    @Bean
+    public ObtainStudent getStudentRepository(StudentDao studentDao){
+        return new StudentRepository(studentDao);
+    }
 }
